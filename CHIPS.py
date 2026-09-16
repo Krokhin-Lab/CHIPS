be052cbd7205738ed97e3d5a259cefc51397b71c
Alexandre Prefontaine <70855231+alexpref711@users.noreply.github.com>
2026-09-15 20:45:57 -0500

    Update documentation and fix main function call

--- CHIPS.py
+++ CHIPS.py
@@ -1,3 +1,19 @@
+# CHIPs — Chromatographic Hydrophobicity Index for Peptides
+# Copyright (C) 2024 Oleg V. Krokhin, Alexandre Préfontaine
+# University of Manitoba, Manitoba Centre for Proteomics and Systems Biology
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 """
 Program: CHIPS.py
 Author: Alexandre Prefontaine
